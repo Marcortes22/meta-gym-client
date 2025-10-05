1d6b643493f1d04e77ce73e3cdbf1f4665ac55a4
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2025-10-05 01:41:35 -0600

    feat/ sidebar de animate ui funcional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,51 @@
-import { Button } from "@/shared/components/ui/button";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarInset, SidebarMenu, SidebarMenuItem, SidebarProvider, SidebarRail, SidebarTrigger } from "@/components/animate-ui/components/radix/sidebar";
 
 export default function Home() {
   return (
     <>
-    <div>
-      <Button>Click me</Button>
-    </div>
+<SidebarProvider>
+  <Sidebar>
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>Item 1</SidebarMenuItem>
+        <SidebarMenuItem>Item 2</SidebarMenuItem>
+        <SidebarMenuItem>Item 3</SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
+    <SidebarContent>
+      <SidebarGroup>
+        <SidebarGroupLabel>Label 1</SidebarGroupLabel>
+        <SidebarMenu>
+          <SidebarMenuItem>Item 1</SidebarMenuItem>
+          <SidebarMenuItem>Item 2</SidebarMenuItem>
+          <SidebarMenuItem>Item 3</SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroup>
+      <SidebarGroup>
+        <SidebarGroupLabel>Label 2</SidebarGroupLabel>
+        <SidebarMenu>
+          <SidebarMenuItem>Item 1</SidebarMenuItem>
+          <SidebarMenuItem>Item 2</SidebarMenuItem>
+          <SidebarMenuItem>Item 3</SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroup>
+    </SidebarContent>
+    <SidebarFooter>
+      <SidebarMenu>
+        <SidebarMenuItem>Item 1</SidebarMenuItem>
+        <SidebarMenuItem>Item 2</SidebarMenuItem>
+        <SidebarMenuItem>Item 3</SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarFooter>
+    <SidebarRail />
+  </Sidebar>
+  <SidebarInset>
+    <SidebarTrigger />
+    <main className="p-4">
+      <h1>Welcome to Meta Gym</h1>
+    </main>
+  </SidebarInset>
+</SidebarProvider>
     </>
   );
 }
